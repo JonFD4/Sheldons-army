4cdfb60bce1bc00b03263f1294ef810b38564414
JonFD4 <jonathanforsondanso@gmail.com>
2023-09-01 11:12:19 +0100

    Making player and computer's logic for game

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -14,4 +14,38 @@ function updateScore() {
     document.querySelector('.cpu-result').textContent = computerScore;
     document.querySelector('.result-details').textContent = `You: ${playerScore} Computer: ${computerScore}`;
     }
-   
+    function showResult(message) {
+        document.querySelector('.result-details').textContent = message;
+        }
+/* Player Game logic and moves left */
+        function playerGame() {
+            const btnOptions = document.querySelectorAll('.choice');
+            btnOptions.forEach(btnOption => {
+            btnOption.addEventListener('click', function() {
+            if (movesLeft > 0) {
+            const playerSelect = btnOption.dataset.name;
+            const computerSelect = computerChoice();
+            movesLeft--;
+            document.getElementById('movesLeft').textContent = movesLeft;
+            console.log(playerSelect);
+            console.log(computerSelect);
+            compareChoices(playerSelect, computerSelect);
+            } else {
+            console.log('Game Over');
+            if (playerScore > computerScore) {
+            alert('You win the game!');
+            } else if (playerScore < computerScore) {
+            alert('Computer wins the game!');
+            } else {
+            alert('It\'s a tie game!');
+            }
+            }
+            });
+            });
+            }
+/* Computer game logic*/
+            function computerChoice() {
+                let choice = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+                const randomBtn = Math.floor(Math.random() * choice.length);
+                return choice[randomBtn];
+                }
