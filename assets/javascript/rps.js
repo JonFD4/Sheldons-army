751a30a4f076ced99897599ce8399010bec5ea87
JonFD4 <jonathanforsondanso@gmail.com>
2023-09-01 11:03:37 +0100

    Creating a function that access divs in html and ouputs playerscore, computerscore and movve made in document

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,6 +1,17 @@
-function inputNames(){ 
+/*function inputNames(){ 
     let firstPlayerName= prompt("Enter your name, first player");
     return document.getElementById("playerName").innerHTML=` ${firstPlayerName} :`;
 }
 inputNames ();
 
+*/
+
+let playerScore=0;
+let computerScore=0;
+let movesLeft =5;
+function updateScore() {
+    document.querySelector('.player-result').textContent = playerScore;
+    document.querySelector('.cpu-result').textContent = computerScore;
+    document.querySelector('.result-details').textContent = `You: ${playerScore} Computer: ${computerScore}`;
+    }
+   
