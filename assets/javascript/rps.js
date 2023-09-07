f97d62db3d49961d98df0f2de1ee928ce5503162
JonFD4 <>
2023-09-07 19:05:09 +0000

    n/a

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -2,10 +2,10 @@
 
 /* Prompt for user name */
 function inputName() {
-  const playerName = prompt("Enter your name, first player");
-  return (document.getElementById(
-    "playerName"
-  ).innerHTML = ` ${playerName} :`);
+    const playerName = prompt("Enter your name, first player");
+    return (document.getElementById(
+        "playerName"
+    ).innerHTML = ` ${playerName} :`);
 }
 
 
@@ -16,9 +16,9 @@ let resetButton = document.getElementById('resetButton');
 
 
 function updateScore() {
-  document.querySelector(".player-result").textContent = playerScore;
-  document.querySelector(".cpu-result").textContent = computerScore;
-  document.querySelector(".result-details").textContent;
+    document.querySelector(".player-result").textContent = playerScore;
+    document.querySelector(".cpu-result").textContent = computerScore;
+    document.querySelector(".result-details").textContent;
 
 }
 /* Function to show message in result */
@@ -27,80 +27,79 @@ function updateScore() {
  * @param {string} message 
  */
 function showResult(message) {
-  document.querySelector(".result-details").textContent = message;
+    document.querySelector(".result-details").textContent = message;
 }
 
 /* Player Game logic and moves left */
 function playGame() {
-  const btnOptions = document.querySelectorAll(".choice");
-  btnOptions.forEach((btnOption) => {
-    btnOption.addEventListener("click", function () {
-      if (movesLeft > 0) {
-        const playerSelect = btnOption.dataset.name;
-        const computerSelect = computerChoice();
-        const computerChoiceElem = document.querySelector(".displayComputerChoice");
-        computerChoiceElem.innerHTML= `Computer chose ${computerSelect}`
+    const btnOptions = document.querySelectorAll(".choice");
+    btnOptions.forEach((btnOption) => {
+        btnOption.addEventListener("click", function () {
+            movesLeft--;
+            document.getElementById("movesLeft").textContent = movesLeft;
+            if (movesLeft > 0) {
+                const playerSelect = btnOption.dataset.name;
+                const computerSelect = computerChoice();
+                const computerChoiceElem = document.querySelector(".displayComputerChoice");
+                computerChoiceElem.innerHTML = `Computer chose ${computerSelect}`;
 
-        movesLeft--;
-       
-        document.getElementById("movesLeft").textContent = movesLeft;
-        console.log(playerSelect);
-        console.log(computerSelect);
-        compareChoices(playerSelect, computerSelect);
-      } else {
-        console.log("Game Over");
-        showResult("Game Over")
-        if (playerScore > computerScore) {
-          console.log("You win the game!");
-          showResult("You win the game!");
-        } else if (playerScore < computerScore) {
-          console.log("Computer wins the game!");
-          showResult("Computer wins the game!");
-        } else {
-          console.log("It's a tie game!");
-          showResult("It's a tie game!")
-        }
-      }
+                console.log(playerSelect);
+                console.log(computerSelect);
+                compareChoices(playerSelect, computerSelect);
+            } else {
+                console.log("Game Over");
+                showResult("Game Over");
+                if (playerScore > computerScore) {
+                    console.log("You win the game!");
+                    showResult("You win the game!");
+                } else if (playerScore < computerScore) {
+                    console.log("Computer wins the game!");
+                    showResult("Computer wins the game!");
+                } else {
+                    console.log("It's a tie game!");
+                    showResult("It's a tie game!");
+                }
+            }
+        });
     });
-  });
 }
 /* Computer game logic*/
 function computerChoice() {
-  let choice = ["rock", "paper", "scissors", "lizard", "spock"];
-  const randomBtn = Math.floor(Math.random() * choice.length);
+    let choice = ["rock", "paper", "scissors", "lizard", "spock"];
+    const randomBtn = Math.floor(Math.random() * choice.length);
 
-  return choice[randomBtn];
+    return choice[randomBtn];
 }
 /* Compare Choice of computer and user*/
 function compareChoices(playerSelect, computerSelect) {
-  switch (true) {
-    case playerSelect === computerSelect:
-      console.log(`It's a tie!`);
-      showResult(`It's a tie!`);
-      break;
-    case (playerSelect === "rock" &&
-      (computerSelect === "scissors" || computerSelect === "lizard")) ||
-      (playerSelect === "paper" &&
-        (computerSelect === "rock" || computerSelect === "spock")) ||
-      (playerSelect === "scissors" &&
-        (computerSelect === "paper" || computerSelect === "lizard")) ||
-      (playerSelect === "lizard" &&
-        (computerSelect === "paper" || computerSelect === "spock")) ||
-      (playerSelect === "spock" &&
-        (computerSelect === "rock" || computerSelect === "scissors")):
-      console.log(`You win!`);
-      showResult(`You win!`);
-      playerScore++;
-      break;
-    default:
-      console.log(`Computer wins!`);
-      showResult(`Computer wins!`);
-      computerScore++;
-  }
+    switch (true) {
+        case playerSelect === computerSelect:
+            console.log(`It's a tie!`);
+            showResult(`It's a tie!`);
+            break;
+        case (playerSelect === "rock" &&
+            (computerSelect === "scissors" || computerSelect === "lizard")) ||
+            (playerSelect === "paper" &&
+                (computerSelect === "rock" || computerSelect === "spock")) ||
+            (playerSelect === "scissors" &&
+                (computerSelect === "paper" || computerSelect === "lizard")) ||
+            (playerSelect === "lizard" &&
+                (computerSelect === "paper" || computerSelect === "spock")) ||
+            (playerSelect === "spock" &&
+                (computerSelect === "rock" || computerSelect === "scissors")):
+            console.log(`You win!`);
+            showResult(`You win!`);
+            playerScore++;
+            break;
+        default:
+            console.log(`Computer wins!`);
+            showResult(`Computer wins!`);
+            computerScore++;
+    }
 
 
 
-  updateScore();
+    updateScore();
 }
 
 
@@ -111,14 +110,14 @@ playGame();
 
 // Option to reset game during or afte play 
 function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-  movesLeft = 5;
-  updateScore();
-  showResult('');
-  document.getElementById(`movesLeft`).textContent = movesLeft;
-  document.querySelector(`.displayComputerChoice`).innerHTML = ``;
-  
-  }
- 
-  resetButton.addEventListener('click', resetGame());
+    playerScore = 0;
+    computerScore = 0;
+    movesLeft = 5;
+    updateScore();
+    showResult('');
+    document.getElementById(`movesLeft`).textContent = movesLeft;
+    document.querySelector(`.displayComputerChoice`).innerHTML = ``;
+
+}
+
+resetButton.addEventListener('click', resetGame());
